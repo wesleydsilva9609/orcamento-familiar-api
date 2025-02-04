a10cd2ba51c2a7cc3364c693de2d653ecd69bc39
wesley <wesleydsilva96@gmail.com>
2025-02-04 16:11:29 -0300

    listar receitas

--- src/main/java/br/com/alura/orcamento_familiar_api/controller/ReceitasController.java
+++ src/main/java/br/com/alura/orcamento_familiar_api/controller/ReceitasController.java
@@ -1,13 +1,13 @@
 package br.com.alura.orcamento_familiar_api;
 
 import jakarta.validation.Valid;
+import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -21,4 +21,10 @@ public class ReceitasController {
     public ResponseEntity cadastrarReceita(@RequestBody @Valid DadosCadastroReceita receita, UriComponentsBuilder uriComponentsBuilder){
         return service.cadastrarReceita(receita,uriComponentsBuilder);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemReceita>> listagem(@PageableDefault(size = 8, sort = "id")Pageable pageable){
+        return
+    }
+
 }
