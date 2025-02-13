4cf868cdc4017332e6862f0b2c39f698ebcb9744
wesley <wesleydsilva96@gmail.com>
2025-02-13 14:54:48 -0300

    criação autenticao service e security configuration

--- src/main/java/br/com/alura/orcamento_familiar_api/controller/UsuarioController.java
+++ src/main/java/br/com/alura/orcamento_familiar_api/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package br.com.alura.orcamento_familiar_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login")
 public class UsuarioController {
+    @Autowired
+    private AuthenticationManager manager;
+
 
 
     @PostMapping
--- src/main/java/br/com/alura/orcamento_familiar_api/entities/Usuario.java
+++ src/main/java/br/com/alura/orcamento_familiar_api/entities/Usuario.java
@@ -2,12 +2,18 @@ package br.com.alura.orcamento_familiar_api.entities;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity(name = "Usuario")
 @Table(name = "usuarios")
 @Getter
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,4 +29,38 @@ public class Usuario {
         this.senha = senha;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
