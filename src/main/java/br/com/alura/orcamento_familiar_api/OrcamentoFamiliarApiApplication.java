dd94bbc35bb975c3eed8607a421550f9583323bb
Wesley D. Silva <wesleydsilva96@gmail.com>
2025-05-27 17:40:46 -0300

    configurando o dockerfile

--- src/main/java/br/com/alura/orcamento_familiar_api/OrcamentoFamiliarApiApplication.java
+++ src/main/java/br/com/alura/orcamento_familiar_api/OrcamentoFamiliarApiApplication.java
@@ -2,8 +2,12 @@ package br.com.alura.orcamento_familiar_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("br.com.alura.orcamento_familiar_api.entities")
+@EnableJpaRepositories("br.com.alura.orcamento_familiar_api.repository")
 public class OrcamentoFamiliarApiApplication {
 
 	public static void main(String[] args) {
